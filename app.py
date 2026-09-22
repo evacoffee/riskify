8ee54f68d4b4ce81feadd8c5e91122222f1d0a47
evmaagca <evmaagca@gmail.com>
2026-09-21 22:02:36 -0400

    commit #22

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 from quant.data import get_data
-from quant.strategy import moving_average_strategy
+from quant.strategy import (moving_average_strategy, momentum_strategy)
 from quant.backtest import run_backtest, calculate_metrics
 
 from quant.backtest import(
@@ -18,36 +18,50 @@ def index():
         ticker = request.form["ticker"]
         start = request.form["start"]
         end = request.form["end"]
+        strategy = request.form["strategy"]
+
         short_window = int(request.form["short_window"])
         long_window = int(request.form["long_window"])
-        if short_window >= long_window:
-            raise ValueError(
-                "short MA must be smaller than long MA."
-            )
+
+        if strategy == "moving_average":
+            if short_window >= long_window:
+                raise ValueError(
+                    "short MA must be smaller than long MA."
+                )
+            
         data = get_data(
             ticker,
             start,
             end
         )
-        data = moving_average_strategy(
-            data, short_window, long_window
-        )
+
+        if strategy == "moving_average":
+            data = moving_average_strategy(data, short_window, long_window)
+        elif strategy == "momentum":
+            data = momentum_strategy(data, 20)
+
         data = run_backtest(data)
         metrics = calculate_metrics(data)
+
         chart_path = "static/performance.png"
+
         create_performance_chart(
             data, chart_path
         )
+
         drawdown_path = "static/drawdown.png"
+
         create_drawdown_chart(
             data, drawdown_path
         )
+
         results = {
             "ticker": ticker,
             "short_window": short_window,
             "long_window": long_window,
             "metrics": metrics
         }
+
     return render_template(
         "index.html",
         results=results
--- quant/strategy.py
+++ quant/strategy.py
@@ -14,3 +14,12 @@ def moving_average_strategy(data, short_window=20, long_window=50):
         data["Short_MA"] < data["Long_MA"], "Signal"
     ] = 0
     return data
+
+def momentum_strategy(data, window=20):
+    data = data.copy()
+    data["Momentum"] = data["Close"].pct_change(window)
+    data["Signal"] = 0
+    data.loc[
+        data["Momentum"] > 0, "Signal"
+    ] = 1
+    return data
